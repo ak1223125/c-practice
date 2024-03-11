09cca28ac2cb89d6ce162a16ed38d5a23981c290
Aman Kumar <ak1223125@gmail.com>
2024-03-11 21:01:40 +0530

    practice of loops

---
+++ pattern printing of increaseing order.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d ", a);
+            a = a+1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
