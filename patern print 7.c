44abf1b9a4105d130b7a1fb74b05d8ce85211591
Aman Kumar <ak1223125@gmail.com>
2024-03-25 10:24:49 +0530

    pattern printing

---
+++ patern print 7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 1; j < 5-i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
